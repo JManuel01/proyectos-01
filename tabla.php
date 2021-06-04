5d4e843115cc31ddaf382eb92eb820ff8a756a30
JManuel01 <83259552+JManuel01@users.noreply.github.com>
2021-06-03 23:05:13 -0500

    Add files via upload

---
+++ tabla.php
@@ -0,0 +1,88 @@
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<?php
+
+include "conexion.php";
+
+$user_id=null;
+$sql1= "select * from invex";
+$query = pg_query($dbconn3,$sql1);
+?>
+
+<?php if($query->pg_num_rows>1):?>
+<table class="table table-bordered table-hover">
+<thead>
+	<th>Campana</th>
+	<th>Panoramix</th>
+	<th>Carrier</th>
+	<th>Ip</th>
+	<th>Estatus</th>
+	<th></th>
+</thead>
+<?php while ($r=$query->pg_fetch_array()):?>
+<tr>
+	<td><?php echo $r["campana"]; ?></td>
+	<td><?php echo $r["panoramix"]; ?></td>
+	<td><?php echo $r["carrier"]; ?></td>
+	<td><?php echo $r["ip"]; ?></td>
+	<td><?php echo $r["estatus"]; ?></td>
+	<td style="width:150px;">
+		<a data-id="<?php echo $r["id"];?>" class="btn btn-edit btn-sm btn-warning">Editar</a>
+		<a href="#" id="del-<?php echo $r["id"];?>" class="btn btn-sm btn-danger">Eliminar</a>
+		<script>
+		$("#del-"+<?php echo $r["id"];?>).click(function(e){
+			e.preventDefault();
+			swal({
+  title: " ESstas Seguro",
+  text: "Se perdera",
+  icon: "warning",
+  buttons: true,
+  dangerMode: true,
+})
+.then((p) => {
+  if (p) {
+    swal("Poof! Your imaginary file has been deleted!", {
+      icon: "success",
+    });
+  } else {
+    swal("Your imaginary file is safe!");
+  }
+});
+			if(p){
+				$.get("./php/eliminar.php","id="+<?php echo $r["id"];?>,function(data){
+					loadTabla();
+				});
+			}
+
+		});
+		</script>
+	</td>
+</tr>
+<?php endwhile;?>
+</table>
+<?php else:?>
+	<p class="alert alert-warning">No hay resultados</p>
+<?php endif;?>
+  <!-- Modal -->
+  <script>
+  	$(".btn-edit").click(function(){
+  		id = $(this).data("id");
+  		$.get("./php/formulario.php","id="+id,function(data){
+  			$("#form-edit").html(data);
+  		});
+  		$('#editModal').modal('show');
+  	});
+  </script>
+  <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <h4 class="modal-title">Actualizar</h4>
+        </div>
+        <div class="modal-body">
+        <div id="form-edit"></div>
+        </div>
+
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal -->
